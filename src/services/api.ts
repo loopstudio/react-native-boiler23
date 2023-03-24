c61944c1d41dab9f04843c766458c8bc64ca3f41
Genaro Lorio <glorio@loopstudio.dev>
2023-03-24 11:07:45 -0300

    add axios middleware

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,18 @@
-// TODO: Here should go the axios middleware
+import axios from 'axios';
+import applyCaseMiddleware from 'axios-case-converter';
 
 const apiClient = () => {
-  return;
+  const api = axios.create({
+    baseURL: 'https://some-domain.com/api/', // here go the baseURL of your api,
+    headers: {
+      'Content-Type': 'application/json',
+      accept: '*/*',
+      // Authorization: "Bearer token" --> here go you bearer token
+    },
+    params: {},
+  });
+  // you can set your header like this too: api.defaults.headers.common.Authorization = 'Bearer token'
+  return api;
 };
 
-export default apiClient;
+export default applyCaseMiddleware(apiClient());
