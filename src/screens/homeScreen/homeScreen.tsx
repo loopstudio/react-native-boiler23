e89fdda47d4df831f3004bb4e4537215d140114d
Andrés Menchaca <andresfullstack@gmail.com>
2023-04-11 10:42:21 -0300

    Update homeScreen.tsx

--- src/screens/homeScreen/homeScreen.tsx
+++ src/screens/homeScreen/homeScreen.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Home from '../../features/home/home';
 
 const HomeScreen = () => {
-  return <Home data-test-id="homeTest" />;
+  return <Home data-testId="home_test" />;
 };
 
 export default HomeScreen;
