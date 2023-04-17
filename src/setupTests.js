67d2eec408824a3cae2922373718aceb53c80bce
Genaro Lorio <glorio@loopstudio.dev>
2023-04-17 12:54:34 -0300

    delete async storage jest mocked

--- src/setupTests.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-undef */
-import 'react-native-gesture-handler/jestSetup';
-
-import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
-
-jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
